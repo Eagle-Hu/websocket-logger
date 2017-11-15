1e977af2cb4ae7a547e82c3c6a41762c94a4739e
Yanguang <huyanguang@jiuedai.com>
2017-11-15 19:07:21 +0800

    add exception

--- src/main/java/com/example/websocket/logger/LoggerFilter.java
+++ src/main/java/com/example/websocket/logger/LoggerFilter.java
@@ -1,6 +1,9 @@
 package com.example.websocket.logger;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.classic.spi.StackTraceElementProxy;
+import ch.qos.logback.classic.spi.ThrowableProxy;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 import org.springframework.stereotype.Component;
@@ -9,6 +12,9 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * Created by HuYanGuang on 2017/10/31.
@@ -27,6 +33,23 @@ public class LoggerFilter extends Filter {
         LoggerMessage loggerMessage = new LoggerMessage(event.getMessage(), formatter.format(dateTime),
                 event.getThreadName(), event.getLoggerName(), event.getLevel().levelStr);
         LoggerQueue.getInstance().push(loggerMessage);
+
+        IThrowableProxy iThrowableProxy = event.getThrowableProxy();
+        while (Objects.nonNull(iThrowableProxy)) {
+            LoggerMessage exception = new LoggerMessage();
+            exception.setBody(iThrowableProxy.getClassName().concat(": ").concat(iThrowableProxy.getMessage()));
+            LoggerQueue.getInstance().push(exception);
+
+            StackTraceElementProxy[] stackTraceElements = iThrowableProxy.getStackTraceElementProxyArray();
+            Stream.of(stackTraceElements).forEach(stackTraceElementProxy -> {
+                StackTraceElement stackTraceElement = stackTraceElementProxy.getStackTraceElement();
+                LoggerMessage message = new LoggerMessage();
+                message.setBody("---- at ".concat(stackTraceElement.toString()));
+                LoggerQueue.getInstance().push(message);
+            });
+
+            iThrowableProxy = iThrowableProxy.getCause();
+        }
         return FilterReply.ACCEPT;
     }
 }
